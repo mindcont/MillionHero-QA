f8205f820c2b9286bf9e3d4f197738fea3471337
張正軒 <mindcont@users.noreply.github.com>
2018-01-15 20:34:44 +0800

    20180115-《百万英雄》题库更新

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# ixigua-qa
-西瓜视频《百万英雄》历次题库
+# 《百万英雄》题库
+
+![](https://img.shields.io/badge/百万英雄-题库-brightgreen.svg)
+![](https://mindcont.com/bigdata/static/img/build-with-love.svg)
+
+> 《百万英雄》是一档全民知识互动游戏，在《百万英雄》里每场12道题目全部回答正确的人，将瓜分奖金。
+
+![](http://static.mindcont.com/blog/images/resources/ixigua-qa.png)
+
+题库持续更新ing, 喜欢请给我小星星噢~
+## Install
+
+```css
+pip install requests
+```
+use python in windows press `win + R ` or in ubunut press `ctrl + alt +t` to open the terminal
+
+```css
+chmod +x million-answer-spider.py
+python million-answer-spider.py
+```
+the result in some filename like `百万英雄历次答题-20180114.txt`
+```json
+
+2018年1月12日	21:00场 300万
+====================================================
+1	下列哪一项不是我国十二生肖中的动物?	猫
+2	以下不是新疆籍贯的90后小花是?	关晓彤
+3	六分之五这个分数中,6是分母,那5是什么?	分子
+4	下列哪种动物是我国特有的?	大熊猫
+5	特色面食“肉夹馍”是哪个地区的美食?	陕西
+6	以下球类运动中,按球体积大小排序,错误的是?	足球>乒乓球>网球
+7	“天生我才必有用,千金散尽还复来”的作者是谁?	李白
+8	《十面埋伏》是我国哪种传统乐器的名曲?	琵琶
+9	下列哪位球员有在NBA打球的经历?	易建联
+10	湖南省境内旅游景点风景秀丽,名胜众多,其中不包括?	乌镇
+11	一般情况下,家用冰箱冷藏室的温度维持在?	2c到10°c
+12	中国神话中有“龙生九子”一说,以下属于这“九子的是?	饕餮
+====================================================
+
+```
+enjoy and have fun :heart: ~
---
+++ million-answer-spider.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# 本脚本实现 收集百万英雄 历次答题的题库
+# writed by 張正軒
+# 更多访问  http://blog.mindcont.com
+#
+
+import requests
+import json
+from datetime import datetime
+import urllib
+import codecs
+import sys
+import re
+import time
+import urlparse
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def getAndSave():
+
+    # file =codecs.open("百万英雄历次答题.txt","a+","utf-8")
+    saveName = "百万英雄历次答题-" + datetime.now().strftime("%Y%m%d") + ".txt"
+    file =codecs.open(saveName,"w","utf-8")
+    host = 'http://answer.sm.cn/answer/detail?format=json&activity=million'
+
+    for sid_num in range(1,100):
+        url = host + '&sid=' + str(sid_num)
+        rawData = requests.post(url)
+        mjson = json.loads(rawData.text)
+        question = mjson['data']['question']
+
+        if question:
+            print sid_num
+            # print rawData.text
+            tempDate =''
+            q_info = mjson['data']['info']
+            # 保存日期、场次、奖金池信息
+            print >>file, "2018年".decode("utf-8") + q_info['date']+ '\t' + q_info['order'] +' '+ q_info['reward']
+            print >>file, "===================================================="
+
+            # 遍历 12个问题及其答案
+            for q_id in range(0,len(question)):
+                # 保存到txt
+                print >>file,  str(question[q_id]['id']) + '\t' + question[q_id]['title'] + '\t' + question[q_id]['answer']
+
+            print >>file, "====================================================" +'\n'
+
+    file.close()
+    print 'done!'
+
+if __name__ == '__main__':
+
+    getAndSave()
